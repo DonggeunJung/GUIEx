44879dfec3bd71cef33277128bdb5c8bc2339d69
DonggeunJung <topsan72@gmail.com>
2018-07-18 17:42:42 -0400

    Initial commit

---
+++ Assets/GUIControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUIControl : MonoBehaviour {
+
+	public Text titleText;
+
+	public void OnChangeTextButtonClicked() {
+		titleText.text = "Clicked";
+	}
+
+	public void OnSubSceneButtonClicked() {
+		SceneManager.LoadScene ("Sub");
+	}
+}
